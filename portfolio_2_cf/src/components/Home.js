54ab5350bce776c369a9f59b10204b59437ceb9c
christian-fortin <christian.ep.410@gmail.com>
2022-04-27 12:24:41 -0400

    styled home page

--- portfolio_2_cf/src/components/Home.js
+++ portfolio_2_cf/src/components/Home.js
@@ -8,19 +8,19 @@ const Home = () => {
 
 
 
-    <div className="box-work"><a href="https://docs.google.com/document/d/1RTuiJZeeaS0zX6b9pzjtCkSRCgmj31PcRB6Sq4Yf0IA/edit" target="_blank" rel="noopener noreferrer" className='homeLinks'>Resume</a></div>
+    <div className="box-work"><a href="https://docs.google.com/document/d/1RTuiJZeeaS0zX6b9pzjtCkSRCgmj31PcRB6Sq4Yf0IA/edit" target="_blank" rel="noopener noreferrer" className='homeLinks' id='resumeLink-home'>Resume</a></div>
 
 
 
-    <div className="box-education"><Link to='/Projects' className='homeLinks'>Projects</Link></div>
+    <div className="box-education"><Link to='/Projects' className='homeLinks' id='projectLink-home'>Projects</Link></div>
 
 
 
-    <div className="box-projects"><Link to='/Education' className='homeLinks'>Education</Link></div>
+    <div className="box-projects"><Link to='/Education' className='homeLinks' id='educationLink-home'>Education</Link></div>
 
 
 
-    <div className="box-moreAboutMe"><Link to='/MoreAboutMe' className='homeLinks'>About Me</Link></div>
+    <div className="box-moreAboutMe"><Link to='/MoreAboutMe' className='homeLinks' id='aboutmeLink-home'>About Me</Link></div>
 
 
     </div>
