2bee75d785a18d46845f1f45be6fcb7a1842f5f7
christian-fortin <christian.ep.410@gmail.com>
2022-04-27 11:31:44 -0400

    All set up, starting serious styling

--- portfolio_2_cf/src/components/Education.js
+++ portfolio_2_cf/src/components/Education.js
@@ -1,36 +1,46 @@
 const Education = () => {
   return (
     <div>
+      <div className="ContainerEducation">
+        <div className="educationTitle">
       <h1>Education</h1>
+      </div>
+
+
+      <div className="columContainerEducation">
+      <div className="colum1-education">
+        <div className="highschoolDiv-education">
       <h2>HighSchool</h2>
-      <p>
-        <span>
-          <a
-            href="https://www.eaglehill.school/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            EagleHill School
-          </a>
-        </span>
-        - A special education boarding school which focused primarily on
+      <p> <span> <a href="https://www.eaglehill.school/" target="_blank" rel="noopener noreferrer" > EagleHill School </a></span> - A special education boarding school which focused primarily on
         students with Learning Disabilities, including ADHD, dyslexia, auditory
         processing disorder and more based in Hardwick Massachusets. 
         <br />
         Here I Graduated in the top 5% of my class with honors and a 3.85 GPA. I took the hardest classes in the school, including college level Anatomy, Calculus 2, Reading and Writting critical analysis, and other niche topics in science and history.
       </p>
+      </div>
 
-      <h2>College</h2>
-
-      {/* <span><a href="https://www.rollins.edu/" target="_blank" rel="noopener noreferrer">Rollins College</a></span>, <span><a href="https://www.providence.edu/" target="_blank" rel="noopener noreferrer">Providence College</a></span>,  */}
-
-      <p><span><a href="https://www.pace.edu/" target="_blank" rel="noopener noreferrer">Pace University </a> </span> - A liberal arts University in the heart of downtown Manhattan, where I graduate with a Bachelors of Science in Biology and a 3.48 GPA. There I was mainly focused on plant biology, but enjoyed human biology as well.</p>
-
+      <div className="postCollegeDiv-education">
       <h2>Post College</h2>
       <p><span><a href="https://generalassemb.ly/" target="_blank" rel="noopener noreferrer">General Assembly</a> </span> - A technilogical focused bootcamp offering post graduates an opertunity to step into the Data science, UI/UX, and Software Enginnering fields. 
       <br/> 
       I completed a course in JavaScript basics to get a feel and see if I enjoyed the work. I ended up taking the full 6 month program where I gained 480+ hours of expereince in programing languages and techniques. The course included a range of technologies from HTML, CSS, and JavaScript to learning backends and frameworks like  MongoDB, SQL, Flask, Bootstrap and more. </p>
+      </div>
+      </div>
+      <div className="colum2-education">
+        <div className="collegeDiv-education">
+      <h2>College</h2>
 
+{/* <span><a href="https://www.rollins.edu/" target="_blank" rel="noopener noreferrer">Rollins College</a></span>, <span><a href="https://www.providence.edu/" target="_blank" rel="noopener noreferrer">Providence College</a></span>,  */}
+
+<p><span><a href="https://www.pace.edu/" target="_blank" rel="noopener noreferrer">Pace University </a> </span> - A liberal arts University in the heart of downtown Manhattan, where I graduate with a Bachelors of Science in Biology and a 3.48 GPA. There I was mainly focused on plant biology, but enjoyed human biology as well.</p>
+</div>
+      </div>
+      
+
+     </div>
+
+      
+      </div>
     </div>
   );
 };
--- portfolio_2_cf/src/components/MoreAboutMe.js
+++ portfolio_2_cf/src/components/MoreAboutMe.js
@@ -3,9 +3,13 @@ import React from 'react'
 const MoreAboutMe = () => {
   return (
     <div>
+      <div className='aboutMeTitleDiv'>
       <h1 className='aboutMeTitle'>Get to Know Me</h1>
+      </div>
 
-
+    <div className='columContainerAboutMe'>
+      <div className='colum1-AboutMe'>
+                {/* Card One Col 3 \/ ======================================================*/}
       <div class="flip-card">
   <div class="flip-card-inner">
     <div class="flip-card-front">
@@ -18,6 +22,143 @@ const MoreAboutMe = () => {
     </div>
   </div>
 </div>
+{/* One Card /\ =========================================================*/}
+
+
+    {/* Card Two Col 3  \/ ======================================================*/}
+    <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="img_avatar.png" alt="Avatar" style={{width:"300px", height:"300px"}}/>
+    </div>
+    <div class="flip-card-back">
+      <h1>John Doe</h1>
+      <p>Architect and Engineer</p>
+      <p>We love that guy</p>
+    </div>
+  </div>
+</div>
+{/* One Card /\ =========================================================*/}
+
+
+
+    {/* Card Three Col 3  \/ ======================================================*/}
+    <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="img_avatar.png" alt="Avatar" style={{width:"300px", height:"300px"}}/>
+    </div>
+    <div class="flip-card-back">
+      <h1>John Doe</h1>
+      <p>Architect and Engineer</p>
+      <p>We love that guy</p>
+    </div>
+  </div>
+</div>
+{/* One Card /\ =========================================================*/}
+
+
+
+ {/* Card Four Col 3  \/ ======================================================*/}
+    <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="img_avatar.png" alt="Avatar" style={{width:"300px", height:"300px"}}/>
+    </div>
+    <div class="flip-card-back">
+      <h1>John Doe</h1>
+      <p>Architect and Engineer</p>
+      <p>We love that guy</p>
+    </div>
+  </div>
+</div>
+{/* One Card /\ =========================================================*/}
+
+
+      </div>
+
+      <div className='colum2-AboutMe'>
+      </div>
+
+
+      <div className='colum3-AboutMe'>
+        {/* Card One Col 3 \/ ======================================================*/}
+      <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="img_avatar.png" alt="Avatar" style={{width:"300px", height:"300px"}}/>
+    </div>
+    <div class="flip-card-back">
+      <h1>John Doe</h1>
+      <p>Architect and Engineer</p>
+      <p>We love that guy</p>
+    </div>
+  </div>
+</div>
+{/* One Card /\ =========================================================*/}
+
+
+    {/* Card Two Col 3  \/ ======================================================*/}
+    <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="img_avatar.png" alt="Avatar" style={{width:"300px", height:"300px"}}/>
+    </div>
+    <div class="flip-card-back">
+      <h1>John Doe</h1>
+      <p>Architect and Engineer</p>
+      <p>We love that guy</p>
+    </div>
+  </div>
+</div>
+{/* One Card /\ =========================================================*/}
+
+
+
+    {/* Card Three Col 3  \/ ======================================================*/}
+    <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="img_avatar.png" alt="Avatar" style={{width:"300px", height:"300px"}}/>
+    </div>
+    <div class="flip-card-back">
+      <h1>John Doe</h1>
+      <p>Architect and Engineer</p>
+      <p>We love that guy</p>
+    </div>
+  </div>
+</div>
+{/* One Card /\ =========================================================*/}
+
+
+
+ {/* Card Four Col 3  \/ ======================================================*/}
+    <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+      <img src="img_avatar.png" alt="Avatar" style={{width:"300px", height:"300px"}}/>
+    </div>
+    <div class="flip-card-back">
+      <h1>John Doe</h1>
+      <p>Architect and Engineer</p>
+      <p>We love that guy</p>
+    </div>
+  </div>
+</div>
+{/* One Card /\ =========================================================*/}
+
+
+
+      </div>
+
+    </div>
+
+
+
+
+
+
+
     </div>
     
   )
