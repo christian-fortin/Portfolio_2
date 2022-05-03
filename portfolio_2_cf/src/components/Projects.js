6d4eb99b0374b2838775f2acf20491d54b7e9239
christian-fortin <christian.ep.410@gmail.com>
2022-05-02 22:05:08 -0400

    added project

--- portfolio_2_cf/src/components/Projects.js
+++ portfolio_2_cf/src/components/Projects.js
@@ -21,6 +21,10 @@ const Projects = () => {
           appear in the same order as displayed at the top of the screen. To
           beat the game, you must make it past all three levels. As the
           developer, I have not passed level 3.
+          <br/>
+          <br/>
+
+          <span className='indent'>This project was meant to test my JavaScript, HTML, and CSS skills.</span>
         </p>
         <div className="project-img-container">
           <img
@@ -39,6 +43,10 @@ const Projects = () => {
           MyFoodBlog is a personal application for a user to track homemade
           dishes they create. They can keep track of the recipe,
           ingredients, a name, and even have an image for plating ideas.
+          <br/>
+          <br/>
+
+          <span className='indent'>This project was meant to test my JavaScript, HTML, CSS, as well as designing an API with RESTful routes and a back-end using MongoDB, and express. In extension to the project requirements I learned how to apply user authentication using Bcrypt and how to upload an image using Multer  skills.</span>
         </p>
         <div className="project-img-container">
           <img className="project-img" src="https://github.com/christian-fortin/SEIR_Project_2/raw/main/public/images/Screen%20Shot%202022-04-14%20at%2010.54.50%20AM.png?raw=true" alt="foodblog" />
@@ -58,6 +66,10 @@ const Projects = () => {
           <span className="project-title-in-p"> "LoFive" </span> is a social
           media application for users to share and post about music
           interests, songs, albums, artists, and more.
+          <br/>
+          <br/>
+
+          <span className='indent'>This project was meant to test my JavaScript React, HTML, CSS, as well as designing an API with RESTful routes and a back-end. For this group project my group chose to use MongoDB and Express, a full MERN stack. In extension to the project requirements we created user authentication, and multiple external API's.</span>
         </p>
         <div className="project-img-container">
          <img className="project-img" id='project-img-lofive' src="images/loFiveImg.png" alt="LoFive img"/>
@@ -81,11 +93,45 @@ const Projects = () => {
           were taken to a random website. I wanted to recreate that, however
           with a more social media feel and where users can create their
           own.
+          <br/>
+          <br/>
+
+          <span className='indent'>This project was meant to test my JavaScript, HTML, CSS, as well as designing an API with RESTful routes and a back-end using MongoDB, and Express. In extension to the project requirements I learned more about how to apply user authentication using Bcrypt, improved my clean code skills and my JavaScript logic while building something I thought interesting. </span>
         </p>
         <div className="project-img-container">
           <img
             className="project-img"
             src="https://github.com/christian-fortin/Project_4/raw/main/public/Screen%20Shot%202022-04-06%20at%2010.14.27%20AM.png?raw=true"
+            alt="whatIsThis"
+          />
+        </div>
+      </div>
+
+
+
+
+
+
+      <div className="project-div">
+        <a
+          href="http://tattoo-blog-410.s3-website-us-east-1.amazonaws.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          alt="link to website"
+        >
+          <h4 className="project-title">Tattoo Blog</h4>
+        </a>
+        <p className="project_p">
+
+          <span className="project-title-in-p"> "Tattoo" </span> is
+          a personal website and storage place for some thoughts and ideas. It is mainly a practice in website design as well as I wanted to learn how to deploy a simple project using AWS.
+          
+        </p>
+        <div className="project-img-container">
+          <img
+            className="project-img"
+            id='project-img-tattoo'
+            src="https://github.com/christian-fortin/Tattoo-Ideas/blob/main/chess%20abstract.png?raw=true"
             alt="whatIsThis"
           />
         </div>
